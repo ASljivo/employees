c2c4af8df0cc4274d212a707d0a1dce73322bd92
Armin <armin@email.com>
2022-03-23 00:38:58 +0100

    Handle data if response is faild

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -4,6 +4,7 @@ import useApi from '../hooks/useApi';
 import { Employee } from '../model/employee';
 import EmployeeService from '../services/employes';
 import { EmployeesTable } from '../componets/EmployeesTable';
+import api from '../api/api.json';
 
 export const Dashboard: FC = () => {
   const { fetch, isLoading } = useApi(EmployeeService.list);
@@ -11,7 +12,12 @@ export const Dashboard: FC = () => {
 
   const getEmployees = () => {
     fetch().then(({ data }) => {
-      setEmployees(data.data);
+      console.log('data', data);
+      if (data) {
+        setEmployees(data.data);
+      } else {
+        setEmployees(api.data);
+      }
     });
   };
 
@@ -20,6 +26,10 @@ export const Dashboard: FC = () => {
   }, []);
 
   return (
-    <div>{employees.length > 0 && <EmployeesTable data={employees} isLoadingGet={isLoading} />}</div>
+    <div>
+      {employees.length > 0 && (
+        <EmployeesTable data={employees} isLoadingGet={isLoading} />
+      )}
+    </div>
   );
 };
